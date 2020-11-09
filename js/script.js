f577407129ded6f471f763e1442bc82d2cd8d8e7
Allan-Kleckner <73128542+Allan-Kleckner@users.noreply.github.com>
2020-11-08 18:32:37 -0600

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+console.log('test');
+ $('#retrieveData').on("click", handleGetData)
+  function handleGetData(event){
+	event.preventDefault();
+
+  // Load in the value of the search textbox:
+  const tickerVal = $("#ticker").val()
+console.log (tickerVal);
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?region=US&symbol=${tickerVal}`,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+		"x-rapidapi-key": "de8ce92fe7mshac52192236134f6p16f83bjsn84aa9bfa8d10"
+	}}
+	$.ajax(settings).done(function (response) {
+		$("#close").text(response.prices.close)
+        $("#date").text(response.date)
+        $("#high").text(response.high)
+		$("#low").text(response.low)
+		$("#open").text(response.open)
+		console.log(tickerVal);
+		console.log(close, date, high, low, open);
+	});
+    
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
