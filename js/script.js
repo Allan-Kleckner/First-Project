d6166707ea933f0244e4022ab8e3bac09388214b
allanrkleck <allanrkleck@gmail.com>
2021-01-06 09:27:33 -0600

    update

--- js/script.js
+++ js/script.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-console.log('test');
- $('#retrieveData').on("click", handleGetData)
-  function handleGetData(event){
-	event.preventDefault();
-
-  // Load in the value of the search textbox:
-  const tickerVal = $("#ticker").val()
-console.log (tickerVal);
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?region=US&symbol=${tickerVal}`,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-		"x-rapidapi-key":   "de8ce92fe7mshac52192236134f6p16f83bjsn84aa9bfa8d10"
-	}}
-	$.ajax(settings).done(function (response) {
-		$("#close").text(response.prices[0].close)
-        //$("#date").text(response.prices[0].date)
-        $("#high").text(response.prices[0].high)
-		$("#low").text(response.prices[0].low)
-		$("#open").text(response.prices[0].open)
-		console.log(tickerVal);
-		console.log(response);
-	});
-    
-}
-
-
-///////////////////////////////////////////////////////////////////////////////////////////
-
-=======
 console.log('test');
  $('#retrieveData').on("click", handleGetData)
   function handleGetData(event){
@@ -60,4 +26,3 @@ var settings = {
 	});
     
 }
->>>>>>> 45e185b99cdcfeb0047a976075b5c30c82baa9b4
